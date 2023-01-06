d5ecb80f233dae10cc38f7559ce4cd649c516f4a
Andre Cordeiro <andrecordeirolacerda@gmail.com>
2023-01-06 09:08:03 -0300

    Some improvments and bug fixes

--- src/model/entities/Account.java
+++ src/model/entities/Account.java
@@ -1,29 +1,29 @@
 package model.entities;
 
-import model.exceptions.DomainException;
+import model.exceptions.BusinessException;
 
 public class Account {
-	
+
 	private Integer number;
 	private String holder;
 	private Double balance;
 	private Double withdrawLimit;
-	
-	
-	
+
+	public Account() {
+	}
+
+
 	public Account(Integer number, String holder, Double balance, Double withdrawLimit) {
-		super();
 		this.number = number;
 		this.holder = holder;
 		this.balance = balance;
 		this.withdrawLimit = withdrawLimit;
 	}
-	
+
 
 	public Integer getNumber() {
 		return number;
 	}
-
 
 
 	public void setNumber(Integer number) {
@@ -31,11 +31,9 @@ public class Account {
 	}
 
 
-
 	public String getHolder() {
 		return holder;
 	}
-
 
 
 	public void setHolder(String holder) {
@@ -43,11 +41,9 @@ public class Account {
 	}
 
 
-
 	public Double getBalance() {
 		return balance;
 	}
-
 
 
 	public Double getWithdrawLimit() {
@@ -58,22 +54,20 @@ public class Account {
 	public void deposit(Double amount) {
 		balance = balance + amount;
 	}
-	
+
 	public void withdraw(Double amount) {
+		validateWithdraw(amount);
 		if (amount < getWithdrawLimit() && amount < balance) {
 			balance = balance - amount;
 		}
-		if (amount > getWithdrawLimit()){
-			throw new DomainException("The amount exceeds withdraw limit");
-		}
-		else {
-			throw new  DomainException("Not enough balance");
-		}
-		
 	}
 
-	
-	
-	
-	
+	private void validateWithdraw(double amount) {
+		if (amount > getWithdrawLimit()) {
+			throw new BusinessException("Withdraw error: The amount excede the withdraw limit");
+		} 
+		if (amount > getBalance()) {
+			throw new BusinessException("Withdraw error: Not enough balance");
+		}		
+	}
 }
